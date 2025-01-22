3160c2b67a40d700f12604c9e6c50fc7ee7808bf
VlaBeh <166541377+VlaBeh@users.noreply.github.com>
2025-01-22 20:38:21 +0200

    Solution

--- app/parse.py
+++ app/parse.py
@@ -1,4 +1,10 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, fields, astuple
+import requests
+import bs4
+import csv
+from typing import List
+
+BASE_URL = 'https://quotes.toscrape.com/'
 
 
 @dataclass
@@ -8,8 +14,46 @@ class Quote:
     tags: list[str]
 
 
+QUOTE_FIELDS = [field.name for field in fields(Quote)]
+
+
+def parse_single_quote(quote) -> Quote:
+    tags = [tag.text for tag in quote.select('.tag')]
+
+    return Quote(
+        text=quote.select_one('span.text').text,
+        author=quote.select_one('.author').text,
+        tags=tags,
+    )
+
+
+def get_quotes() -> List[Quote]:
+    quotes = []
+    next_page = BASE_URL
+    while next_page:
+        response = requests.get(next_page)
+        soup = bs4.BeautifulSoup(response.text, 'html.parser')
+        for quote_tag in soup.select(".quote"):
+            quotes.append(parse_single_quote(quote_tag))
+
+        next_button = soup.select_one(".next > a")
+        if next_button:
+            next_page = BASE_URL + next_button["href"]
+        else:
+            next_page = None
+
+    return quotes
+
+
+def quotes_to_csv(quotes: List[Quote], file_name: str) -> None:
+    with open(file_name, 'w', newline='', encoding='utf-8') as f:
+        writer = csv.writer(f)
+        writer.writerow(QUOTE_FIELDS)
+        writer.writerows([astuple(quote) for quote in quotes])
+
+
 def main(output_csv_path: str) -> None:
-    pass
+    quotes_to_csv(get_quotes(), output_csv_path)
 
 
 if __name__ == "__main__":
